62a1327085159bdd57f307d16581289881970922
chilgoe <zonda037@gmail.com>
2024-03-30 14:33:32 +0700

    Fix export ConfigurationKey

--- src/version/1.6/Configuration/index.ts
+++ src/version/1.6/Configuration/index.ts
@@ -3,13 +3,11 @@ import LocalAuthListManagementProfile from './LocalAuthListManagementProfile';
 import ReservationProfile from './ReservationProfile';
 import SmartChargingProfile from './SmartChargingProfile';
 
-const ConfigurationKey = {
+export const ConfigurationKey = {
   ...CoreProfile,
   ...LocalAuthListManagementProfile,
   ...ReservationProfile,
   ...SmartChargingProfile,
 };
-
-export default ConfigurationKey;
 
 export type ConfigurationKey = (typeof ConfigurationKey)[keyof typeof ConfigurationKey];
